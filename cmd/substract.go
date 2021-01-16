49343854f303f7a650833f0b312e7dabb029bfd6
ichsanputr <githubichsan@gmail.com>
2021-01-16 09:39:45 +0700

    Change Description Info CLI

--- cmd/substract.go
+++ cmd/substract.go
@@ -10,7 +10,7 @@ var tes bool
 
 var substractCmd = &cobra.Command{
 	Use:   "reduce",
-	Short: "A brief description of your command",
+	Short: "Reduce your number in here",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
